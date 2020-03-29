8f150bebeaf6460569239c040a71651bcd8fd050
Aditya7689 <60285894+Aditya7689@users.noreply.github.com>
2020-03-29 20:15:01 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,9 @@
 // Your First Program
 
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+public class Hello 
+{
+    public static void main (String args[]) 
+    {
+       System.out.println ("Hello World!");
     }
 }
